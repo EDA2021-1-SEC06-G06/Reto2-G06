a6a35cfca8e293950f2c60b23e30fafb65cb15ef
ThaisTamaio <t.tamaio@uniandes.edu.co>
2021-03-11 23:25:27 -0500

    Se copiaron las funciones del requerimiento 1

--- App/controller.py
+++ App/controller.py
@@ -23,16 +23,203 @@
 import config as cf
 import model
 import csv
+import datetime as dt
 
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
 
-# Inicialización del Catálogo de libros
+
+# Inicialización del Catálogo de vídeos
+
+
+
+def initCatalog():
+    """
+    Llama la funcion de inicializacion del catalogo del modelo.
+
+    Retorna el catálogo.
+    """
+    catalog = model.newCatalog()
+    return catalog
+
+
 
 # Funciones para la carga de datos
 
+
+
+def loadData(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadCategoryID(catalog)
+    loadVideos(catalog)
+
+
+
+
+def loadCategoryID(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Carga las categorías del archivo.
+    """
+    categoryfile = cf.data_dir + 'videos/category-id.csv'
+    input_file = csv.DictReader(open(categoryfile, encoding='utf-8'), delimiter="\t")
+    for category in input_file:
+        filtered_category = {
+            'id': category['id'],
+            'name': (category['name']).replace(" ", '')  # quitar los espacios
+        }
+
+        model.addCategoryID(catalog, filtered_category)
+
+
+
+
+def loadVideos(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Carga todos los vídeos del archivo y los agrega a la lista de vídeos
+    """
+    videosfile = cf.data_dir + 'videos/videos-large.csv'  # videos-large para la entrega
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+
+        filtered_video = {
+            'video_id': video['video_id'],
+            'trending_date': dt.datetime.strptime(video['trending_date'], '%y.%d.%m').date(),
+            'title': video['title'],
+            'channel_title': video['channel_title'],
+            'category_id': int(video['category_id']),
+            'publish_time': dt.datetime.strptime(video['publish_time'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+            'tags': video['tags'],
+            'views': int(video['views']),
+            'likes': int(video['likes']),
+            'dislikes': int(video['dislikes']),
+            'country': video['country'],
+            'dias_t': 1
+        }
+
+
+        model.addVideo(catalog, filtered_video)
+        model.addVideoCountry(catalog, filtered_video['country'], filtered_video)
+
+
+
 # Funciones de ordenamiento
 
+
+
+def sortVideos(catalog, cmp: int):
+    """
+    Args:
+        catalog: Catálogo de videos.
+        cmp: (1) cmpVideosByViews (2) cmpVideosByTitle (3) cmpVideosByID (4) cmpVideosByLikes
+
+    Return:
+        list: Ordena los vídeos según sus views.
+    """
+    return model.sortVideos(catalog, cmp)
+
+
+
 # Funciones de consulta sobre el catálogo
+
+
+
+def primerVideo(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Return:
+        video1: Retorna el primero video cargado en el catálogo.
+    """
+    video1 = model.primerVideo(catalog)
+    return video1
+
+
+
+
+def getVideosByCountry(catalog, countryName):
+    """
+    Args:
+        catalog: Catálogo de videos.
+        countryName: Nombre del país para flitrar los videos.
+
+    Return:
+        list: catálogo filtrado por el nombre del país.
+    """
+    country = model.getVideosByCountry(catalog, countryName)
+    return country
+
+
+
+
+def getVideosByCategory(catalog, categoryName, categoryCatalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+        categoryName: Nombre de la categoría para flitrar los videos.
+
+    Return:
+        list: catálogo filtrado por el nombre de la categoría.
+    """
+    category = model.getVideosByCategory(catalog, categoryName, categoryCatalog)
+    return category
+
+
+
+
+def getVideosByTag(catalog, tag):
+    """
+    Args:
+        catalog: Catálogo de videos
+        tag: Nombre del tag ingresado por el usuario
+
+    Return:
+        tag: Catálogo filtrado de acuerdo con el tag ingresado por parámetro
+    """
+    tag = model.getVideosByTag(catalog, tag)
+    return tag
+
+
+
+# Funciones de operaciones sobre el catálogo
+
+
+
+def masDiasTrending(catalog, llave: int):
+    """
+    Args:
+        catalog: Catálogo ordenado según los Títulos
+        llave: (1) 'title' o (2) 'video_id'
+
+    Return:
+        video_mayor_dias: Video que ha tenido más días de tendencia.
+    """
+    catalog = model.masDiasTrending(catalog, llave)
+    return catalog
+
+
+
+
+def quitarCopiasLikes(ord_videos, size):
+    """
+    Args:
+        ord_videos: Catálogo de videos ordenado.
+
+    Return:
+        ord_videos: Catálogo de videos ordenado y filtrado para que no se repita un mismo video.
+    """
+    return model.quitarCopiasLikes(ord_videos, size)
--- App/view.py
+++ App/view.py
@@ -27,6 +27,7 @@ from DISClib.ADT import list as lt
 assert cf
 
 
+
 """
 La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones y por cada seleccion
@@ -34,24 +35,270 @@ se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
 
+
+
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- REQ. 1: Encontrar buenos videos por categoría y país")
+    print("3- REQ. 2: Encontrar video tendencia por país")
+    print("4- REQ. 3: Encontrar video tendencia por categoría")
+    print("5- REQ. 4: Buscar los videos con más Likes")
+    print("0- Salir")
+
+
+
+
+def printResults(ord_videos, sample=10):
+    """
+    Args:
+        ord_videos: Catálogo de videos ordenados.
+        sample: cantidad de videos para imprimir.
+    """
+    size = lt.size(ord_videos)
+
+    if size > sample:
+        print("Los primeros {0} vídeos ordenados son:\n".format(sample))
+
+        i = 1
+
+        while i <= sample:
+
+            video = lt.getElement(ord_videos, i)
+
+            print("Fecha de tendencia: {0}   Título: {1}   Canal: {2}   Fecha de publicación: {3}   Visitas: {4}   Likes: {5}   Dislikes: {6}\n".format(video['trending_date'], video['title'], video['channel_title'], video['publish_time'], video['views'], video['likes'], video['dislikes']))
+
+            i += 1
+
+
+
+
+def printReqCuatro(ord_videos, sample=10):
+    """
+    Args:
+        ord_videos: Catálogo de videos ordenados.
+        sample: cantidad de videos para imprimir.
+
+    Imrpime el requerimiento 4
+    """
+    size = lt.size(ord_videos)
+
+    if size > sample:
+        print("Los primeros {0} vídeos ordenados son:\n".format(sample))
+
+        i = 1
+
+        while i <= sample:
+
+            video = lt.getElement(ord_videos, i)
+
+            if (i + 1) <= size and video['title'] == lt.getElement(ord_videos, i + 1)['title']:  # Para que no se imprima el mismo vídeo consecuntivamente
+                i += 1
+                sample += 1
+
+            print("Título: {0}   Canal: {1}   Fecha de publicación: {2}   Visitas: {3}   Likes: {4}   Dislikes: {5}   Tags: {6}\n".format(video['title'], video['channel_title'], video['publish_time'], video['views'], video['likes'], video['dislikes'], video['tags']))
+
+            i += 1
+
+
+
+
+def printCountryData(country):
+    """
+    Args:
+        country: Nombre del país.
+
+    Imrpime el país y la cantidad de vídeos en ese país.
+    """
+
+    if country:
+        print("Nombre del país: {0}\n".format(country['name']))
+        print('Cantidad de vídeos en este país: {0}\n'.format(lt.size(country['videos'])))
+    else:
+        print("No se encontró el país")
+
+
+
+
+def printCategoryData(category):
+    """
+    Args:
+        category: Nombre de la categría.
+
+    Imrpime la categoría y la cantidad de vídeos en esa categoría.
+    """
+    if category:
+        print('Nombre de la categoría: {0}\n'.format(category['name']))
+        print("Cantidad de vídeos en la categoría: {0}\n".format(lt.size(category['videos'])))
+    else:
+        print("No se encontró")
+
+
+
+
+def printPrimerVideo(video):
+    """
+    Args:
+        video: Nombre del primer video cargado en el catálogo.
+
+    Return:
+        video1: Retorna el primero video cargado en el catálogo.
+    """
+
+    if video:
+        return("Fecha de tendencia: {0}   Título: {1}   Canal: {2}   Fecha de publicación: {3}   Visitas: {4}   Likes: {5}   Dislikes: {6}".format(video['trending_date'], video['title'], video['channel_title'], video['publish_time'], video['views'], video['likes'], video['dislikes']))
+    else:
+        return("No se encontró el primero video")
+
+
+
+
+def printCategoryID(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Imprime el nombre y el ID de la categoría.
+    """
+    if catalog:
+        print("El ID y el nombre de las categorias el lo siguiente:\n")
+        for category in lt.iterator(catalog["category_id"]):
+
+            print("{0} --- {1}".format(category['category_id'], category['name']))
+
+
+
+
+def initCatalog():
+    """
+    Inicializa el catálogo de videos.
+    """
+    return controller.initCatalog()
+
+
+
+
+def loadData(catalog):
+    """
+    Args:
+        catalog: Catálogo de videos.
+
+    Carga los videos en la estructura de datos.
+    """
+    controller.loadData(catalog)
+
+
+
 
 catalog = None
+default_limit = 1000
+sys.setrecursionlimit(default_limit * 10)
+
+
 
 """
 Menu principal
 """
+
+
+
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
+
+
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
 
+        # Se inicializa el catálogo.
+        catalog = initCatalog()
+
+        # Se cargan los videos en la estructura de datos.
+        loadData(catalog)
+
+        print("Videos cargados: {0}".format(lt.size(catalog['videos'])))
+
+        print("Categorías cargadas: {0}".format(lt.size(catalog['category_id'])))
+
+        print("El primero video es:\n{0}\n".format(printPrimerVideo(controller.primerVideo(catalog))))
+
+        printCategoryID(catalog)
+
+
+
     elif int(inputs[0]) == 2:
-        pass
+
+        countryName = input("Ingrese el nombre del país que desea:\n~ ")
+
+        countryCatalog = controller.getVideosByCountry(catalog, countryName)  # Nuevo catálogo filtrado del país elegido
+        printCountryData(countryCatalog)
+
+        # Inputs secundarios del usuario
+
+
+        categoryName = input("Ingrese el nombre de la categoría que desea:\n~ ")
+
+        categoryCatalog = controller.getVideosByCategory(countryCatalog, categoryName, catalog)  # Mirar parámetros
+
+        printCategoryData(categoryCatalog)  # Se imprime la información filtrada por categoría y país
+
+
+        cantidad_videos = int(input("Ingrese la cantidad de vídeos que desea listar:\n~ "))
+
+
+
+        result = controller.sortVideos(categoryCatalog, 1)  # Ordenamiento por views
+
+        printResults(result, sample=cantidad_videos)
+
+
+
+    elif int(inputs[0]) == 3:
+
+        countryName = input("Ingrese el nombre del país que le interesa:\n~ ")
+
+        countryCatalog = controller.getVideosByCountry(catalog, countryName)  # Nuevo catálogo filtrado del país elegido
+        printCountryData(countryCatalog)
+
+        ordenados = controller.sortVideos(countryCatalog, 3)  # Vídeos ordenados según su ID
+
+        video = controller.masDiasTrending(ordenados, 2)  # No funciona
+
+        print("El vídeo con más días de tendencia en el país {0} fue:\nTítulo: {1} -- Canal: {2} -- País: {3} -- Días de Tendencia: {4}\n".format(countryName, video['title'], video['channel_title'], video['country'], video['dias_t']))
+
+
+
+    elif int(inputs[0]) == 4:
+
+        categoryName = input("Ingrese el nombre de la categoría que le interesa:\n~ ")
+
+        categoryCatalog = controller.getVideosByCategory(catalog, categoryName, catalog)  # Catálogo filtrado por la categoría
+        printCategoryData(categoryCatalog)
+
+        ordenados = controller.sortVideos(categoryCatalog, 2)  # Vídeos ordenados según su título
+
+        video = controller.masDiasTrending(ordenados, 1)
+
+        print("El vídeo con más días de tendencia en la categoría {0} fue:\nTítulo: {1} -- Canal: {2} -- ID de la Categoría: {3} -- Días de Tendencia: {4}\n".format(categoryName, video['title'], video['channel_title'], video['category_id'], video['dias_t']))
+
+
+
+    elif int(inputs[0]) == 5:
+        countryName = input("Ingrese el nombre del país que le interesa:\n~ ")
+        countryCatalog = controller.getVideosByCountry(catalog, countryName)
+
+        printCountryData(countryCatalog)
+
+        tag = input("Ingrese el tag que desea consultar:\n~ ")
+        size = int(input("Ingrese la cantidad de vídeos que desea listar:\n~ "))
+
+        tagsCatalog = controller.getVideosByTag(countryCatalog, tag)
+
+        likesCatalog = controller.sortVideos(tagsCatalog, 4)
+
+        printReqCuatro(likesCatalog, size)
+
+
 
     else:
         sys.exit(0)
